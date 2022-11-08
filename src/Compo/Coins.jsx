5172c62ab24b8610deba6d0181884e09b9ea7c2c
mallickraja1909 <mallickraja1909@gmail.com>
2022-11-08 17:13:41 +0530

    i

--- src/Compo/Coins.jsx
+++ src/Compo/Coins.jsx
@@ -101,38 +101,5 @@ if(error) return <Error message={'Error While Fetching Coins......'}/>
 };
 
 
-// const ExchangeCard = ({ name, img, price, url }) => (
-//   <a href={url} target={"blank"}>
-//     <VStack
-//       w={"52"}
-//       shadow={"lg"}
-//       boxShadow={'dark-lg'}
-//       p={"8"}
-//       borderRadius={"lg"}
-//       transition={"all 0.5s"}
-//       m={"4"}
-//       css={{
-//         "&:hover": {
-//           transform: "scale(1.1)",
-//         },
-//       }}
-//     >
-//       <Image
-//         src={img}
-//         w={"10"}
-//         h={"10"}
-//         objectFit={"contain"}
-//         alt={"Exchange"}
-//       />
-//       <Heading size={"md"} noOfLines={1}>
-//        Rs: {price}
-//       </Heading>
-
-//       <Text noOfLines={1}>{name}</Text>
-//     </VStack>
-//   </a>
-
-// );
-
 
 export default Coins
